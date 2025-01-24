cf3595d017edcc6fd654e7637f1497ae9decc9b9
Graham Arnett <GrahamKentArnett@outlook.com>
2025-01-24 14:01:40 -0500

    init commit, work in progress

---
+++ HangManApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HangManApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to my Hangmanapp");
+            Random random = new Random();
+            string choice;
+            //lets put the array for possible answers here for now
+            string[] answers = {"London", "Paris","Berlin","Tokyo","Beijing","Seoul","Brussels"};
+            do
+            {
+
+                Console.Write("\n\nWould you like to go again? (y/n): ");
+                choice = Console.ReadLine().ToLower();
+            } while (choice == "y");
+        }
+    }
+}
