ceb8ae931b90af3b4fe5c5f67b6966c77d0a6ca1
Graham Arnett <GrahamKentArnett@outlook.com>
2025-01-25 14:06:45 -0500

    little further on the logic, have it so it prints out an underscore for each letter of the string

--- HangManApp/Program.cs
+++ HangManApp/Program.cs
@@ -11,7 +11,13 @@
             string[] answers = {"London", "Paris","Berlin","Tokyo","Beijing","Seoul","Brussels"};
             do
             {
-
+                int pickedIndex = random.Next(0,answers.Length);
+                string pickedAnswer = answers[pickedIndex];
+                //Console.WriteLine(pickedAnswer); was used to make sure the below logic worked
+                for(int i = pickedAnswer.Length -1; i >= 0; i--)
+                {
+                    Console.Write('_');//to show how many characters and also the empty spaces
+                }
                 Console.Write("\n\nWould you like to go again? (y/n): ");
                 choice = Console.ReadLine().ToLower();
             } while (choice == "y");
