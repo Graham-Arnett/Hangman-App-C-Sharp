ae778872c270c1cfcb2c85e83801dd0236413695
Graham Arnett <GrahamKentArnett@outlook.com>
2025-01-26 16:31:42 -0500

    I have logic to check how long your entry is

--- HangManApp/Program.cs
+++ HangManApp/Program.cs
@@ -9,15 +9,31 @@
             string choice;
             //lets put the array for possible answers here for now
             string[] answers = {"London", "Paris","Berlin","Tokyo","Beijing","Seoul","Brussels"};
+            //char guess;
+            bool gameEnd = false;
             do
             {
+                while(gameEnd == false) 
+                    { 
                 int pickedIndex = random.Next(0,answers.Length);
                 string pickedAnswer = answers[pickedIndex];
                 //Console.WriteLine(pickedAnswer); was used to make sure the below logic worked
+                Console.Write("\nGuess a letter: ");
+                string guess = Console.ReadLine();
+                if(guess.Length == 1) 
+                        { 
                 for(int i = pickedAnswer.Length -1; i >= 0; i--)
                 {
+                    //if()
                     Console.Write('_');//to show how many characters and also the empty spaces
                 }
+                        }
+                    else
+                    {
+                        Console.WriteLine("Invalid input, enter a single letter");
+                    }
+
+                    }
                 Console.Write("\n\nWould you like to go again? (y/n): ");
                 choice = Console.ReadLine().ToLower();
             } while (choice == "y");
